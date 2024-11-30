71fa3de70678ddc50d9bef264a63ec0bd9c3ae28
artkacp <125892314+artkacp@users.noreply.github.com>
2024-11-30 21:45:18 +0100

    Finish builtin pwd

---
+++ src/builtins/builtin_pwd.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_pwd.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akacprzy <akacprzy@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/17 22:16:38 by akacprzy          #+#    #+#             */
+/*   Updated: 2024/11/26 22:25:51 by akacprzy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	bin_pwd(void)
+{
+	char	cwd[PATH_MAX];
+
+	if (getcwd(cwd, PATH_MAX))
+	{
+		ft_putendl_fd(cwd, 1);
+		return (SUCCESS);
+	}
+	else
+		return (ERROR);
+}
